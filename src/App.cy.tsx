531b6a4149b1798319ecfe1db4f7ba9cdd0c9584
Barthélémy Ledoux <bart@cypress.io>
2024-09-26 09:48:23 +0200

    add a percy Snapshot

--- src/App.cy.tsx
+++ src/App.cy.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import App from './App'
+import App from "./App";
 
-describe('<App />', () => {
-  it('renders', () => {
+describe("<App />", () => {
+  it("renders", () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<App />)
-  })
-})
+    cy.mount(<App />);
+
+    cy.contains("App").should("be.visible");
+
+    cy.percySnapshot("App");
+  });
+});
