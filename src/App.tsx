4f66bbd986451aaf642b60fb13d2887cd3186676
Barthélémy Ledoux <bart@cypress.io>
2024-09-26 09:37:25 +0200

    first commit
    - webpack 5
    - typescript 5
    - tailwindcss 4
    - react 18

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <main>
+      <p className="text-red-500 font-bolder line-through text-3xl">App</p>
+      <Footer />
+    </main>
+  );
+}
